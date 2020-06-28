9b28037481b28d56533cce2da02bb9555203ead5
ahmed-elnashar <a7medelnashar@hotmail.com>
2020-06-28 19:29:04 +0200

    building an array from scratch

---
+++ arrays/arrays.js
@@ -0,0 +1,50 @@
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+
+  get(index) {
+    return this.data[index];
+  }
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+
+  pop() {
+    const lastItem = this.data[this.data[this.length - 1]];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+
+  delete(index) {
+    const item = this.data[index];
+    this.shiftItems(index);
+    return item;
+  }
+
+  shiftItems(index) {
+    for (let i = index; i < this.length; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+}
+// a b   d e
+// 1 2 3 4 5
+
+const newArray = new MyArray();
+newArray.push('hi');
+newArray.push('you');
+newArray.push('!');
+// newArray.pop();
+console.log(newArray);
+newArray.delete(1);
+
+console.log('---------------');
+console.log(newArray);
